a800ac7bf4687010faa9de91b7a4d3bf5e2f02b8
SakthiVigneshwar <sakthi34534l@gmail.com>
2025-08-07 19:16:06 +0530

    hahha

--- src/main/java/com/cluegame/funnygame/controller/ClueController.java
+++ src/main/java/com/cluegame/funnygame/controller/ClueController.java
@@ -10,7 +10,7 @@ import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/clue")
-@CrossOrigin(origins = "*") // Change this for production
+@CrossOrigin(origins = "https://guyzzkodle-frontend.vercel.app")
 public class ClueController {
 
     @Autowired
